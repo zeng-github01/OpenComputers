6f3634ac82b137ab4d373dcea384150d6c0e5415
Adrian Siekierka <kontakt@asie.pl>
2025-01-05 14:15:19 +0100

    fix #3729

--- src/main/scala/li/cil/oc/client/Manual.scala
+++ src/main/scala/li/cil/oc/client/Manual.scala
@@ -86,7 +86,13 @@ object Manual extends ManualAPI {
 
   override def contentFor(path: String): java.lang.Iterable[String] = {
     val cleanPath = com.google.common.io.Files.simplifyPath(path)
-    val language = FMLCommonHandler.instance.getCurrentLanguage
+    val language = try {
+      FMLCommonHandler.instance.getCurrentLanguage
+    } catch {
+      case t: Throwable =>
+        OpenComputers.log.warn("The game threw an error when querying current language.", t)
+        FallbackLanguage
+    }
     contentForWithRedirects(cleanPath.replaceAll(LanguageKey, language)).
       orElse(contentForWithRedirects(cleanPath.replaceAll(LanguageKey, FallbackLanguage))).
       orNull
